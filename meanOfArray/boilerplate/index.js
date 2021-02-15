ec93e116c615bae5abb543e4aa056f0bc7f5ff1a
Tae Wilson <taewilson78@gmail.com>
2021-02-15 14:14:37 -0500

    comment out kata

--- meanOfArray/boilerplate/index.js
+++ meanOfArray/boilerplate/index.js
@@ -29,7 +29,24 @@ function getAverage(scores){
   console.log("answer", getAverage([4,5,6,3,1,3,5,6]))
   
 
+//   function getAverage(scores){
+//     //need to get average of scores - add all and divide by length of array, round dowm with math.floor 
+//     let sum=0 // *running total
+//     for (let i = 0; i < scores.length ; i++){
+//         //run for the lenth of the array 
+//         //bracket notation 
+//         sum = sum + scores [i]
+        
+//     }
+//     }
+//    //find the average of the scores
+//    //console.log(sum/scores.length)
+//    //round down to nearest integer
+//    return Math.floor(sum/scores.length)
+    
+//   }
   
+//   console.log("answer", getAverage([4,5,6,3,1,3,5,6]))
 
 
 
