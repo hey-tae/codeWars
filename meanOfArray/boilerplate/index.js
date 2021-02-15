250987a75669f70c31f411ebc09d9819fb6106f5
Tae Wilson <taewilson78@gmail.com>
2021-02-15 14:11:13 -0500

    remove comment

--- meanOfArray/boilerplate/index.js
+++ meanOfArray/boilerplate/index.js
@@ -29,9 +29,7 @@ function getAverage(scores){
   console.log("answer", getAverage([4,5,6,3,1,3,5,6]))
   console.log("answer", getAverage([4,5,6,3,1,3,5,6]))
 
-  //new array
-
-  console.log("answer", getAverage([4,5,6,3,1,3,5,6]))
+  
 
 
 
