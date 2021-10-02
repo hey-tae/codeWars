b232e5bd078c6cafa0d0b4aa4fcaf223d0a04400
Tae Wilson <taewilson78@gmail.com>
2021-10-02 18:20:58 -0400

    new challenge

---
+++ pallindrome/index.js
@@ -0,0 +1,16 @@
+/* CHALLENGE
+Given a string of text, return true or false indicating whether or not the text is a palindrome.
+e.g palindromeChecker('racecar') // will return true
+*/
+
+
+
+
+function palindromeChecker(text) {
+    text = text.toLowerCase()
+   return  text.split('').reverse().join('') === text
+}
+
+
+
+module.exports = palindromeChecker;
